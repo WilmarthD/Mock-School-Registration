1633b998696deab0decf1d5da0dc1c08ad2bc47f
WilmarthD <dwilmar1@binghamton.edu>
2023-02-16 16:37:24 -0500

    Added regex for each field on top

--- scheduler.cpp
+++ scheduler.cpp
@@ -3,17 +3,32 @@
 #include <stdlib.h>
 #include <vector>
 #include <sstream>
+#include <regex>
 #include "Parser.h"
 
 using namespace std;
 
 void Parse(string inputString, vector<string> &tokensVector);
 void showPrompt();
+int DeterminErrors(vector<string>& input);
+void ErrorOutput(int errorNumber);
 
 int main()
 {
     string userInput = "";
+    string inputWhitespaceChecked ="";
     vector<string> inputArgs;
+
+    //Regex checking input(need to add second '\' before d compared to regex101)
+    regex buildCheck("/build \\d{6} [A-Z]{2,4} [100-700]{3} [a-zA-Z0-9]+( [a-zA-Z0-9_]+)*/g");
+    regex cancelCheck("/cancel \\d{6}/g");
+    regex enrollCheck("/enroll B[0-9]{8} [a-z][a-zA-Z0-9]* [a-zA-Z]* [a-zA-Z]*/g");
+    regex addCheck("/add B[0-9]{8} [0-9]{6}/g");
+    regex dropCheck("/drop B[0-9]{8} [0-9]{6}/g");
+    regex rosterCheck("/roster [0-9]{6}/g");
+    regex scheduleCheck("/schedule B[0-9]{8}/g");
+
+    regex whiteSpaceChecker("(\t)( )");
 
     //Give user prompt on commands
     showPrompt();
@@ -21,6 +36,9 @@ int main()
     //Loop for each instruction in the line split by spaces
     while(getline(cin, userInput))
     {
+        //Replace the arbitrary whitespace in user input
+        inputWhitespaceChecked = regex_replace(userInput, whiteSpaceChecker, " ");
+        
         //Split input into seperate strings
         Parse(userInput, inputArgs);
 
@@ -28,6 +46,7 @@ int main()
         if (inputArgs.empty())
         {
             showPrompt();
+            cout << ": ";
             continue;
         }
 
@@ -38,12 +57,29 @@ int main()
         }
         else if(inputArgs.at(0) == "build")
         {
-            cout << "build course ";
-            for (int i = 1; i < (int)inputArgs.size(); i++)
+            if((int)inputArgs.size() != 5)
             {
-                cout << inputArgs.at(i) << " ";
+                ErrorOutput(2);
+                continue;
             }
-            cout << endl;
+
+            
+            /*else if(inputArgs.at(1).size() != 6)
+            {
+                ErrorOutput(3);
+                continue;
+            }
+            for(int i = 0; i < (int)inputArgs.at(1).size();i++)
+            {
+                if(!isdigit(inputArgs.at(1).at(i)))
+                {
+                    ErrorOutput(3);
+                    continue;
+                }
+            }*/
+            
+            cout << "build course " << inputArgs.at(1) << " " << inputArgs.at(2) << " ";
+            cout << inputArgs.at(3) << " " << inputArgs.at(4) << endl;
         }
         else if(inputArgs.at(0) == "cancel")
         {
@@ -111,4 +147,53 @@ void showPrompt() {
             "       \"schedule <bnumber>\"" << std::endl <<
             "       \"quit\"]" << endl <<
             ": ";
+}
+//Function to determine if there is an error from user, outputting the
+// respective error number*****************************************************************might not use
+int DeterminErrors(vector<string>& input)
+{
+    int errorNum = 0;
+
+
+
+    return errorNum;
+}
+//Function to output diffrent error messages depending on int
+//Key: 1 = unrecognized command
+//     2 = too many/few arguments
+//     3 = Malformed B number/CRN
+//     4 = Incorrect types
+//     5 = empty lines
+//     6 = Irregular whitespace input
+//     etc... (to be announced)
+void ErrorOutput(int errorNumber)
+{
+    if(errorNumber != 0)
+    {
+        cout << "Error: ";
+
+        switch(errorNumber)
+        {
+            case 1:
+                cout << "Unrecognized commands" << endl;
+                break;
+            case 2:
+                cout << "Too many or too few arguments" << endl;
+                break;
+            case 3:
+                cout << "Malformed B number or CRN" << endl;
+                break;
+            case 4:
+                cout << "Incorrect types (such as letters inserted where numbers were expected)" << endl;
+                break;
+            case 5:
+                cout << "Empty lines" << endl;
+                break;
+            case 6:
+                cout << "Irregular whitespace in input" << endl;
+                break;
+        }
+    }
+    
+    
 }
